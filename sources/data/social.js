c0f27a9cdbd91fbbc7c6611315d865cab7a8e55c
dhiaa <dhiaguetiti@gmail.com>
2026-06-10 22:51:44 +0100

    Add sources/data/social.js

---
+++ sources/data/social.js
@@ -0,0 +1,6 @@
+export default [
+    { name: 'GitHub', url: 'https://github.com/dhiaktiti', align: 'right' },
+    { name: 'Mail', url: 'mailto:dhiaguetiti@gmail.com', align: 'right' },
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/dhia-eddine-ktiti/', align: 'right' },
+    { name: 'Phone', url: 'tel:+21692454120', align: 'left' },
+]
