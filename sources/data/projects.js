1f3da68bb296f68cffc821e5e943ef2f55c957e8
dhiaa <dhiaguetiti@gmail.com>
2026-06-10 22:51:45 +0100

    Add sources/data/projects.js

---
+++ sources/data/projects.js
@@ -0,0 +1,96 @@
+export default [
+    {
+        title: 'Security Audit PFE',
+        titleSmall: [ 'Security', 'Audit' ],
+        url: 'https://github.com/dhiaktiti',
+        attributes:
+        {
+            role: [ 'Security Auditor', 'Developer' ],
+            with: [ 'M. Aziz Mehri' ]
+        },
+        distinctions: [],
+        images:
+        [
+            'security-audit-1.ktx',
+            'security-audit-2.ktx',
+            'security-audit-3.ktx',
+        ]
+    },
+    {
+        title: 'Sotunol Platform',
+        titleSmall: [ 'Sotunol', 'Platform' ],
+        url: 'https://github.com/dhiaktiti',
+        attributes:
+        {
+            role: [ 'Full-Stack Dev', 'AI Developer' ]
+        },
+        distinctions: [],
+        images:
+        [
+            'sotunol-1.ktx',
+            'sotunol-2.ktx',
+            'sotunol-3.ktx',
+        ]
+    },
+    {
+        title: 'AI Doc Pipeline',
+        titleSmall: [ 'AI Doc', 'Pipeline' ],
+        url: 'https://github.com/dhiaktiti',
+        attributes:
+        {
+            role: 'AI Developer'
+        },
+        distinctions: [],
+        images:
+        [
+            'ai-pipeline-1.ktx',
+            'ai-pipeline-2.ktx',
+        ]
+    },
+    {
+        title: 'POS Map System',
+        titleSmall: [ 'POS Map', 'System' ],
+        url: 'https://github.com/dhiaktiti',
+        attributes:
+        {
+            role: [ 'Full-Stack Dev', 'WebGL Developer' ]
+        },
+        distinctions: [],
+        images:
+        [
+            'pos-map-1.ktx',
+            'pos-map-2.ktx',
+        ]
+    },
+    {
+        title: 'Sawarni App',
+        titleSmall: [ 'Sawarni', 'App' ],
+        url: 'https://github.com/dhiaktiti',
+        attributes:
+        {
+            role: [ 'Full-Stack Dev' ]
+        },
+        distinctions: [],
+        images:
+        [
+            'sawarni-1.ktx',
+            'sawarni-2.ktx',
+        ]
+    },
+    {
+        title: 'Radio ISTIC',
+        titleSmall: [ 'Radio', 'ISTIC' ],
+        url: 'https://github.com/dhiaktiti',
+        attributes:
+        {
+            role: 'Full-Stack Dev',
+            at: 'ISTIC University'
+        },
+        distinctions: [],
+        images:
+        [
+            'radio-istic-1.ktx',
+            'radio-istic-2.ktx',
+        ]
+    },
+]
