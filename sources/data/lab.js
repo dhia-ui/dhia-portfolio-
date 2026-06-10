13cc87537ea3f1b1006e033e0fe238b9b01a0d9e
dhiaa <dhiaguetiti@gmail.com>
2026-06-10 22:51:46 +0100

    Add sources/data/lab.js

---
+++ sources/data/lab.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title: 'Security Audit',
+        url: 'https://github.com/dhiaktiti',
+        image: 'security-audit',
+        video: 'media/videos/security-audit.mp4',
+    },
+    {
+        title: 'Sotunol Platform',
+        url: 'https://github.com/dhiaktiti',
+        image: 'sotunol',
+        video: 'media/videos/sotunol.mp4',
+    },
+    {
+        title: 'AI Data Pipeline',
+        url: 'https://github.com/dhiaktiti',
+        image: 'ai-pipeline',
+        video: 'media/videos/ai-pipeline.mp4',
+    },
+    {
+        title: 'POS Map System',
+        url: 'https://github.com/dhiaktiti',
+        image: 'pos-map',
+        video: 'media/videos/pos-map.mp4',
+    },
+    {
+        title: 'Sawarni App',
+        url: 'https://github.com/dhiaktiti',
+        image: 'sawarni',
+        video: 'media/videos/sawarni.mp4',
+    },
+    {
+        title: 'Radio ISTIC',
+        url: 'https://github.com/dhiaktiti',
+        image: 'radio-istic',
+        video: 'media/videos/radio-istic.mp4',
+    }
+]
