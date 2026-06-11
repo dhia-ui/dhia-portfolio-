b71b2b4030013fceb96d80b884755409b9c42100
Dhia Ktiti <dhia-ui@users.noreply.github.com>
2026-06-11 02:16:20 +0100

    Fix Netlify production deploy

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,8 @@ export default {
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: false // Add sourcemap
+        sourcemap: false, // Add sourcemap
+        target: 'esnext'
     },
     plugins:
     [
